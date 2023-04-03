a96a83ae744b72f06bc4963b965f12c0ce331757
DigitalAthletics <125799868+DigitalAthletics@users.noreply.github.com>
2023-04-03 22:48:57 +0300

    Update bot.py
    Stable version, problem found

--- bot.py
+++ bot.py
@@ -1,35 +1,45 @@
 import discord
-# import os
 import sys
-# import requests
 import json
-# from discord.ext import commands
 import main
 import Logger_custom
 import threading
-import asyncio
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
 # default value
 game_mode = 'Pathfinder_simplified'
 ready = 0
+
+tasks = []
+threads = []
+threads_new = []
+counter = 0
+task_for_deletion = 0
+
 
 @client.event
 async def on_ready():
     print("Table reader bot is ready")
 
+
 @client.event
 async def on_message(message):
     global game_mode
+    global counter
+    global tasks
+    global threads
+    global task_for_deletion
+
     if message.author == client.user:
         return
     # --------------------------------------
-    if message.content.startswith('$hello'):
-        task = asyncio.create_task(message.channel.send('Hello!'))
-        await asyncio.sleep(1)
-        task.cancel()          
+    elif message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
     # --------------------------------------
-    if message.content.startswith('$Add new player:'):
+    elif message.content.startswith('$Add new player:'):
         Str = message.content.replace('$Add new player:', '')
         Str = Str.replace('$Add new player:', '')
         Data = Str.split(",")
@@ -42,113 +52,83 @@ async def on_message(message):
             d[Data[0]] = Data[1]
             with open('Players.json', 'w') as f:
                 json.dump(d, f, indent=2)
-                # print(Data)
                 Logger_custom.AppendLog("Player added " + Data[0])
-                task = asyncio.create_task(message.channel.send('Player added,' + ' player name:' + Data[0]))
-                await asyncio.sleep(1)
-                task.cancel()                 
+                await message.channel.send('Player added,' + ' player name:' + Data[0])
     # --------------------------------------
     # if message.content.startswith('$REBOOT'):
-        # await message.channel.send('Reboot done')
-        # Logger_custom.AppendLog("Reboot call")
-        # os.system('python bot.py')
-        # sys.exit()
+    # await message.channel.send('Reboot done')
+    # Logger_custom.AppendLog("Reboot call")
+    # os.system('python bot.py')
+    # sys.exit()
     # --------------------------------------
-    if message.content.startswith('$EXIT123a098'):
-        task = asyncio.create_task(message.channel.send('Exit done'))
-        await asyncio.sleep(1)
-        task.cancel()        
+    elif message.content.startswith('$EXIT123a098'):
+        await message.channel.send('Exit done')
         Logger_custom.AppendLog("Exit call")
         sys.exit()
     # --------------------------------------
-    if message.content.startswith('$Pathfinder_old'):
+    elif message.content.startswith('$Pathfinder_old'):
         game_mode = 'Pathfinder_old'
-        task = asyncio.create_task(message.channel.send('game mode set to "Pathfinder_old"'))
-        await asyncio.sleep(1)
-        task.cancel()
+        await message.channel.send('game mode set to "Pathfinder_old"')
     # --------------------------------------
-    if message.content.startswith('$Pathfinder_simplified'):
+    elif message.content.startswith('$Pathfinder_simplified'):
         game_mode = '$Pathfinder_simplified'
-        task = asyncio.create_task(message.channel.send('game mode set to "$Pathfinder_simplified"'))
-        await asyncio.sleep(1)
-        task.cancel()
+        await message.channel.send('game mode set to "$Pathfinder_simplified"')
     # --------------------------------------
-    if message.content.startswith('$CUBE_TEST'):
+    elif message.content.startswith('$CUBE_TEST'):
         i = 0
         while i < 10:
-            task = asyncio.create_task(message.channel.send("iteration" + " " + str(i) + " " + ":" + main.CubeSim(1, 20, 0, "-")))
-            await asyncio.sleep(1)
-            task.cancel()   
+            await message.channel.send("iteration" + " " + str(i) + " " + ":" + main.CubeSim(1, 20, 0, "-"))
             i = i + 1
     # --------------------------------------
-    if message.content.startswith('$do'):
+    elif message.content.startswith('$do'):
         message.content.replace('$do', '')
         Str = message.content
         Str = Str.replace('$do', '')
         Data = Str.split(",")
         d = json.load(open("Players.json"))
-        # print(Data[0])
         Val = Data[0].replace(' ', '')
-        # print(Val)
         if Val in d:
-            task = asyncio.create_task(message.channel.send(main.What_Do(d[Val], Data[1], game_mode)))
-            await asyncio.sleep(1)
-            task.cancel()             
+            await message.channel.send(main.What_Do(d[Val], Data[1], game_mode))
     # --------------------------------------
-    if message.content.startswith('$weaponacc'):
+    elif message.content.startswith('$weaponacc'):
         message.content.replace('$weaponacc', '')
         Str = message.content
         Str = Str.replace('$weaponacc', '')
         Data = Str.split(",")
         d = json.load(open("Players.json"))
-        # print(Data[0])
         Val = Data[0].replace(' ', '')
-        # print(Val)
         if Val in d:
-            task = asyncio.create_task(message.channel.send(main.Find_Val(d[Val], Data[1], game_mode, "acc")))
-            await asyncio.sleep(1)
-            task.cancel()            
+            await message.channel.send(main.Find_Val(d[Val], Data[1], game_mode, "acc"))
     # --------------------------------------
-    if message.content.startswith('$weapondmg'):
+    elif message.content.startswith('$weapondmg'):
         message.content.replace('$weapondmg', '')
         Str = message.content
         Str = Str.replace('$weapondmg', '')
         Data = Str.split(",")
         d = json.load(open("Players.json"))
-        # print(Data[0])
         Val = Data[0].replace(' ', '')
-        # print(Val)
         if Val in d:
-            task = asyncio.create_task(message.channel.send(main.Find_Val(d[Val], Data[1], game_mode, "dmg")))
-            await asyncio.sleep(1)
-            task.cancel()
+            await message.channel.send(main.Find_Val(d[Val], Data[1], game_mode, "dmg"))
     # --------------------------------------
-    if message.content.startswith('$skill'):
+    elif message.content.startswith('$skill'):
         message.content.replace('$skill', '')
         Str = message.content
         Str = Str.replace('$skill', '')
         Data = Str.split(",")
         d = json.load(open("Players.json"))
-        # print(Data[0])
         Val = Data[0].replace(' ', '')
-        # print(Val)
-        # print(Data[1])
         if Val in d:
             Data[1] = Data[1].lower()
             if Data[1] == "инициатива" or Data[1] == "стойкость" or Data[1] == "реакция" or Data[1] == "воля":
-                task = asyncio.create_task(message.channel.send(main.What_Do(d[Val], Data[1], game_mode)))
-                await asyncio.sleep(1)
-                task.cancel()
+                was_cancelled = 0
+                await message.channel.send(main.What_Do(d[Val], Data[1], game_mode))
             else:
-                # print(Data[1])
-                task = asyncio.create_task(message.channel.send(main.Find_Val(d[Val], Data[1], game_mode, "skill")))
-                await asyncio.sleep(1)
-                task.cancel()
-    # --------------------------------------
-    Logger_custom.AppendLog("Thread count: "+ str(threading.active_count()))    
+                await message.channel.send(main.Find_Val(d[Val], Data[1], game_mode, "skill"))
 
-# print(os.getenv('TOKEN'))
-if ready==0:
-    client.run(" ")
-    on_ready()
-    ready = 1
+    # --------------------------------------
+    print("Thread count: " + str(threading.active_count()))
+
+    # Logger_custom.AppendLog("Thread count: "+ str(threading.active_count()))
+    # Logger_custom.AppendLog("Cancel status: " + str(was_cancelled))
+
+client.run("OTY3NjgxMzY3MjU1MTU4ODI0.GiUaX7.ZJjvadARA026wuDDnzUo9IKb1EsIfTUUoC7S9w")
